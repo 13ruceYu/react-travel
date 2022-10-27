8068d8e068b991b7544822d7e1e2b60ca6060098
13ruceYu <bruceyuhongbo@gmail.com>
2022-10-27 18:32:32 +0800

    feat: organize Header and Footer

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('让旅游更幸福', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/让/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
-import logo from './assets/logo.svg';
-import styles from './App.module.css';
-import { Layout, Typography, Input } from 'antd'
+import React from "react";
+import styles from "./App.module.css";
+import { Header, Footer } from './components'
 
 function App() {
   return (
-    <div className={styles['App']}>
-      <Layout.Header>
-        <img src={logo} alt="logo" />
-        <Typography.Title level={3}>摸鱼网</Typography.Title>
-        <Input.Search placeholder='touch fish'></Input.Search>
-      </Layout.Header>
+    <div className={styles.App}>
+      <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
