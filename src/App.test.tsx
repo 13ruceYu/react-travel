e87ef7a2c576a9e77e5ae2e47e3d7868dd7050cd
13ruceYu <bruceyuhongbo@gmail.com>
2022-10-28 23:40:01 +0800

    feat: add react-router-dom

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('让旅游更幸福', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/让/i);
-  expect(linkElement).toBeInTheDocument();
-});
