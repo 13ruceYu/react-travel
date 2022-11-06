518b2b2312f5be095be1cf32812467cb78d62d2f
13ruceYu <bruceyuhongbo@gmail.com>
2022-11-06 15:05:01 +0800

    feat: use RTK

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Spin, Row, Col, Divider, Typography, Anchor, Menu } from "antd";
@@ -6,6 +6,9 @@ import styles from "./DetailPage.module.css";
 import { Header, Footer, ProductIntro, ProductComments } from "../../components";
 import { DatePicker } from "antd";
 import { commentMockData } from "./mockup";
+import { productDetailSlice } from '../../redux/productDetail/slice'
+import { useSelector } from '../../redux/hooks'
+import { useDispatch } from 'react-redux'
 
 const { RangePicker } = DatePicker;
 
@@ -15,26 +18,29 @@ type MatchParams = {
 
 export const DetailPage: React.FC = () => {
   const { touristRouteId } = useParams<MatchParams>();
-  const [loading, setLoading] = useState<boolean>(true);
-  const [product, setProduct] = useState<any>(null);
-  const [error, setError] = useState<string | null>(null);
+  // const [loading, setLoading] = useState<boolean>(true);
+  // const [product, setProduct] = useState<any>(null);
+  // const [error, setError] = useState<string | null>(null);
+  const loading = useSelector(state => state.productDetail.loading)
+  const error = useSelector(state => state.productDetail.error)
+  const product = useSelector(state => state.productDetail.data)
+
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true);
+      dispatch(productDetailSlice.actions.fetchStart())
       try {
         const { data } = await axios.get(
           `http://123.56.149.216:8080/api/touristRoutes/${touristRouteId}`
         );
-        setProduct(data);
-        setLoading(false);
-      } catch (error) {
-        setError(error instanceof Error ? error.message : "error");
-        setLoading(false);
+        dispatch(productDetailSlice.actions.fetchSuccess(data))
+      } catch (error: any) {
+        dispatch(productDetailSlice.actions.fetchFail(error.message))
       }
     };
     fetchData();
-  }, [touristRouteId]);
+  }, [dispatch, touristRouteId]);
   if (loading) {
     return (
       <Spin
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,16 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import languageReducer from './language/languageReducer'
 import { actionLog } from './middlewares/actionLog';
 import recommendProductsReducer from './recommendProducts/recommendProductsReducer'
+import { combineReducers } from '@reduxjs/toolkit'
+import {productDetailSlice} from './productDetail/slice'
+
 
 const rootReducer = combineReducers({
   language: languageReducer,
-  recommendProducts: recommendProductsReducer
+  recommendProducts: recommendProductsReducer,
+  productDetail: productDetailSlice.reducer
 })
 
 const store = createStore(rootReducer, applyMiddleware(thunk, actionLog))
