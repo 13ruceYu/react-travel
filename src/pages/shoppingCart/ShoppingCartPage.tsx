5662517fa1c9d39af494861d8952f769daa2a435
13ruceYu <bruceyuhongbo@gmail.com>
2022-11-09 22:39:20 +0800

    feat: shopping cart slice

--- src/pages/shoppingCart/ShoppingCartPage.tsx
+++ src/pages/shoppingCart/ShoppingCartPage.tsx
@@ -1,5 +1,28 @@
-import React from 'react'
+import React from "react";
+import styles from "./ShoppingCartPage.module.css";
+import { MainLayout } from "../../layouts/mainLayout";
+import { Row, Col, Affix } from "antd";
+// import { ProductList, PaymentCard } from "../../components";
 
-export const ShoppingCartPage: React.FC = () => {
-  return <div>shoppingCart</div>
-}
+export const ShoppingCartPage: React.FC = (props) => {
+  return (
+    <MainLayout>
+      <Row>
+        {/* 购物车清单 */}
+        <Col span={16}>
+          <div className={styles["product-list-container"]}>
+            {/* <ProductList /> */}
+          </div>
+        </Col>
+        {/* 支付卡组件 */}
+        <Col span={8}>
+          <Affix>
+            <div className={styles["payment-card-container"]}>
+              {/* <PaymentCard /> */}
+            </div>
+          </Affix>
+        </Col>
+      </Row>
+    </MainLayout>
+  );
+};
---
+++ src/redux/shoppingCart/slice.ts
@@ -0,0 +1,105 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+interface ShoppingCartState {
+  items: any[],
+  error: string | null,
+  loading: boolean
+}
+
+const initialState: ShoppingCartState = {
+  loading: true,
+  error: null,
+  items: []
+}
+
+export const getShoppingCart = createAsyncThunk(
+  "shoppingCart/getShoppingCart",
+  async (jwt: string, thunkAPI) => {
+    const { data } = await axios.get(
+      `http://123.56.149.216:8080/api/shoppingCart`,
+      {
+        headers: {
+          Authorization: `bearer ${jwt}`
+        }
+      }
+    );
+    return data.shoppingCart.items
+  }
+)
+
+export const addShoppingCartItem = createAsyncThunk(
+  "shoppingCart/addShoppingCartItem",
+  async (parameters: { jwt: string, touristRouteId: string }, thunkAPI) => {
+    const { data } = await axios.post(
+      `http://123.56.149.216:8080/api/shoppingCart/items`,
+      {
+        touristRouteId: parameters.touristRouteId
+      },
+      {
+        headers: {
+          Authorization: `bearer ${parameters.jwt}`
+        }
+      }
+    );
+    return data.shoppingCart.items
+  }
+)
+
+export const clearShoppingCartItem = createAsyncThunk(
+  "shoppingCart/clearShoppingCartItem",
+  async (parameters: { jwt: string, itemIds: number[] }, thunkAPI) => {
+    return await axios.delete(
+      `http://123.56.149.216:8080/api/shoppingCart/items/(${parameters.itemIds.join(',')})`,
+      {
+        headers: {
+          Authorization: `bearer ${parameters.jwt}`
+        }
+      }
+    );
+  }
+)
+
+export const shoppingCartSlice = createSlice({
+  name: "shoppingCart",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getShoppingCart.pending.type]: (state) => {
+      state.loading = true
+    },
+    [getShoppingCart.fulfilled.type]: (state, action) => {
+      state.loading = false;
+      state.items = action.payload
+      state.error = null
+    },
+    [getShoppingCart.rejected.type]: (state, action: PayloadAction<string | null>) => {
+      state.loading = false
+      state.error = action.payload
+    },
+    [addShoppingCartItem.pending.type]: (state) => {
+      state.loading = true
+    },
+    [addShoppingCartItem.fulfilled.type]: (state, action) => {
+      state.loading = false;
+      state.items = action.payload
+      state.error = null
+    },
+    [addShoppingCartItem.rejected.type]: (state, action: PayloadAction<string | null>) => {
+      state.loading = false
+      state.error = action.payload
+    },
+    [clearShoppingCartItem.pending.type]: (state) => {
+      state.loading = true
+    },
+    [clearShoppingCartItem.fulfilled.type]: (state,) => {
+      state.loading = false;
+      state.items = []
+      state.error = null
+    },
+    [clearShoppingCartItem.rejected.type]: (state, action: PayloadAction<string | null>) => {
+      state.loading = false
+      state.error = action.payload
+    },
+  }
+})
